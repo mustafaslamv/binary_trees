60d7f9f1f6ee91b6b5b665a6c1bc410d9e7faf92
mustafa slama <ayman1slama@gmail.com>
2023-11-02 19:07:39 +0200

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,22 +1,30 @@
 #include "binary_trees.h"
 
+void traverse(const binary_tree_t *tree, size_t *counter);
 /**
- * binary_tree_nodes - Count nodes with at least one child in a tree.
- * @tree: Pointer to the root of the tree.
- * Return: Number of nodes with at least one child.
+ * binary_tree_nodes - counts the nodes with at least 1 child in a tree
+ * @tree: the root of the tree
+ * Return: number of nodes with at least 1 child
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (0);
+	size_t counter = 0;
 
-	size_t nodes = 0;
+	traverse(tree, &counter);
+	return (counter);
+}
 
+/**
+ * traverse - get the number of nodes that have at least 1 child
+ * @tree: the root of the tree
+ * @counter: pointer to a var to store number of nodes
+ */
+void traverse(const binary_tree_t *tree, size_t *counter)
+{
+	if (!tree)
+		return;
 	if (tree->left || tree->right)
-		nodes++;
-
-	nodes += binary_tree_nodes(tree->left);
-	nodes += binary_tree_nodes(tree->right);
-
-	return (nodes);
+		(*counter)++;
+	traverse(tree->left, counter);
+	traverse(tree->right, counter);
 }
