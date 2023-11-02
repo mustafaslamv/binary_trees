b47409bd66760bd150bdd937d11085f3cad576ff
mustafa slama <ayman1slama@gmail.com>
2023-11-02 18:50:30 +0200

    heigh

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,11 +12,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	else
+		left_height = 0;
+
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+	else
+		right_height = 0;
 
 	if (left_height > right_height)
-		return (left_height + 1);
+		return (left_height);
 	else
-		return (right_height + 1);
+		return (right_height);
 }
