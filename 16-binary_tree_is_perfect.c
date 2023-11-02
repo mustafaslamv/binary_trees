03f2b4fd64dcb469e955a2f2852be954b6cf7ad6
mustafa slama <ayman1slama@gmail.com>
2023-11-02 19:15:20 +0200

    ّ

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,36 +1,40 @@
 #include "binary_trees.h"
 
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t get_size(binary_tree_t *tree, size_t *counter);
+size_t binary_tree_size(const binary_tree_t *tree);
+
 /**
- * binary_tree_is_perfect - Check if a binary tree is perfect.
- * @tree: Pointer to the root node of the tree.
- * Return: 1 if the tree is perfect, 0 otherwise.
+ * binary_tree_is_perfect - checks if a binary tree is full
+ * @tree: root node
+ * Return: 1 is_perfect otherwise 0
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height_of_tree, size_of_tree, i, two_pow_h = 1;
+	size_t hight_of_tree, size_of_tree, i, two_pow_h = 1;
 
 	if (tree == NULL)
 		return (0);
 
-	height_of_tree = binary_tree_height(tree);
+	hight_of_tree = binary_tree_height(tree);
 	size_of_tree = binary_tree_size(tree);
 
-	for (i = 0; i <= height_of_tree; i++)
+	for (i = 0; i <= hight_of_tree; i++)
 		two_pow_h *= 2;
 
 	return (size_of_tree == two_pow_h - 1);
 }
 
 /**
- * binary_tree_height - Calculate the height of a binary tree.
- * @tree: Pointer to the root node.
- * Return: Height of the tree.
+ * binary_tree_height - height of a binary tree
+ * @tree: pointer to tree node
+ * Return: height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
 	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
@@ -40,19 +44,36 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_size - Calculate the size of a binary tree.
- * @tree: Pointer to the root node.
- * Return: Size of the tree.
+ * binary_tree_size - the size of the tree
+ * @tree: the root of the tree
+ * Return: size of the tree
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t left_size = 0, right_size = 0;
 
 	if (tree == NULL)
 		return (0);
-
 	left_size = get_size(tree->left, &left_size);
 	right_size = get_size(tree->right, &right_size);
 
 	return (1 + right_size + left_size);
+}
+
+/**
+ * get_size - the size of the tree
+ * @tree: the root of the tree
+ * @counter: number of nodes of the tree
+ * Return: size of the tree
+ */
+
+size_t get_size(binary_tree_t *tree, size_t *counter)
+{
+	if (!tree)
+		return (0);
+	(*counter)++;
+	get_size(tree->left, counter);
+	get_size(tree->right, counter);
+	return (*counter);
 }
